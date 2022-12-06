f28ef7e92e86caebafdeadea8bd5d4de8c0e97a7
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2022-12-06 14:06:31 -0500

    Add Discord Rich Presence
    - Adds several methods for Discord Rich Presence
    - By default only shows that the player is in the menu
    - Themers can use the new GAMESTATE methods to configure how a player's discord status is shown
    - Name of methods is purely just placeholder for now
    
    (cherry picked from commit 9e30851973c7e3d2fafd0960e866259159aa2fdc)
    
    (cherry picked from commit e3cc3f4e7a62903cece40a24546b1cb871649328)
    (cherry picked from commit 10ca77059ec933d91eed2fa3a2355c02ea235207)
    
    # Conflicts:
    #	StepmaniaCore.cmake

--- src/StepMania.cpp
+++ src/StepMania.cpp
@@ -331,6 +331,7 @@ void ShutdownGame()
 	RageUtil::SafeDelete( FILEMAN );
 	RageUtil::SafeDelete( LUA );
 	RageUtil::SafeDelete( HOOKS );
+	Discord_Shutdown();
 }
 
 static void HandleException( const RString &sError )
