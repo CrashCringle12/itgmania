920fb106291bcd0c56ab60165ab862ae0fe74f51
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-01-14 21:48:04 -0500

    Revert "Add Discord Rich Presence"
    This reverts commit 04d2ba17b63162a7fa874dd1fd82e9757cb5a296.

--- src/StepMania.cpp
+++ src/StepMania.cpp
@@ -331,7 +331,6 @@ void ShutdownGame()
 	RageUtil::SafeDelete( FILEMAN );
 	RageUtil::SafeDelete( LUA );
 	RageUtil::SafeDelete( HOOKS );
-	Discord_Shutdown();
 }
 
 static void HandleException( const RString &sError )
