56fc085b709df10858d1c4339e57da13d9e164b9
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-01-15 17:58:06 -0500

    Remove other unneeded files

--- src/generated/verstub.cpp
+++
@@ -1,8 +0,0 @@
-extern int const sm_version_major = 5;
-extern int const sm_version_minor = 1;
-extern int const sm_version_patch = 0;
-extern char const * const sm_version_git_hash = "08e271d602";
-extern char const * const sm_version_traditional = "5.1.0";
-extern char const * const version_date = "20251203";
-extern char const * const version_time = "20:45:06 -05:00";
-extern char const * const product_version = "5.1-git-08e271d602";
